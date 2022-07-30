b65bdc4fc952b25745e40c928b8e8f7e2fc555a9
unknown <p080621001@bussan.co.id>
2022-07-30 14:03:32 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter as Router, } from 'react-router-dom'
 
 import AnimationRoute from './components/AnimationRoute';
+
 function App() {
   return (
     <Router>
@@ -10,6 +11,7 @@ function App() {
      </Router>
     
   );
+ 
 }
 
 export default App;
--- src/pages/CreateEvent.js
+++ src/pages/CreateEvent.js
@@ -2,21 +2,34 @@ import React, { Component } from 'react'
 import CardTask from '../components/CardTask'
 import Sidebar from '../components/Sidebar'
 import TodayDate from '../components/TodayDate'
+import {Calendar} from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
+import '../asset/css/CreateEvent.css'
+import { CreateIcon } from '../asset/image/Index'
+import Form from 'react-bootstrap/Form';
+import CardForm from '../components/CardForm'
+
+
 
  class CreateEvent extends Component {
-  render() {
+   render() {
     return (
       <div className='listEvent'>
         <Sidebar />
-        <div className="listEvent-continer2">
-          <p className="list-p">Morning NICK!</p>
-         <TodayDate />
-
-         <p className="list-p">Your Task</p>
-         <CardTask />
+        <div style={{paddingLeft:80}}>
+          <p className="list-p">Morning Aldy Anugrasdah!</p>
+          <div className="cont-date">
+        <div style={{width:"65px", height:'65px', backgroundColor:'#192049', borderRadius:50, justifyContent:'center', alignItems:'center', display:'flex'}}>
+          <img src={CreateIcon} className="img-date" />
         </div>
-        <div className="listEvent-continer3">
-          <h1>asdasdasdasdasd</h1>
+        <text style={{fontWeight:'bold', color:"#192049", fontSize:20, marginLeft: 30, marginTop:15}}>Create New Event</text>
+      </div>
+         <div style={{display:'flex',  width:'50%', height:'40%', borderRadius:20, justifyContent:'center', alignItems:'center', marginTop:30}}>
+          <Calendar style={{borderRadius:20}} />
+         </div>
+        </div>
+        <div className="createEvent-continer3">
+         <CardForm/>
         </div>
       </div>
     )
